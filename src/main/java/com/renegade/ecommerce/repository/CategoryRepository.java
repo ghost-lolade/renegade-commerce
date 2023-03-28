73fdacc9c292c3ffd3fc42b95bf0cf496cd286a3
Renegade <lolade.wahab@mypatricia.co>
2023-03-28 19:58:58 +0100

    add category repository, extends JPARepository

---
+++ src/main/java/com/renegade/ecommerce/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.renegade.ecommerce.repository;
+
+import com.renegade.ecommerce.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+    Category findCategoryByName(String categoryName);
+}
