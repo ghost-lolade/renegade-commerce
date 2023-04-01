42c7133aab770ed3ba67c8a79af5e10bad07f36e
Renegade <lolade.wahab@mypatricia.co>
2023-04-02 00:40:46 +0100

    add cors configuration

---
+++ src/main/java/com/renegade/ecommerce/config/webconfig.java
@@ -0,0 +1,20 @@
+package com.renegade.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+class Webconfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
